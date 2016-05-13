14644ddf28a3c6bb9ca45ef849234c955b4b230d
Erica Garcia <me@athenas.space>
2016-05-13 18:19:12 -0500

    optimized RandomKicks, removed lastFM.py
    LastFM.py has been removed. It was unstable, unoptimized, untested
    for a long time, and might be incompatible with the current API.

--- randomkicks.py
+++ randomkicks.py
@@ -13,66 +13,40 @@ import random
 
 textfiledir = hexchat.get_info("configdir")
 
-def kbquote(word, word_eol, userdata):
-	if len(word) <= 1:
-		hexchat.emit_print("Notice", __module_name__, "No arguments given.")
-		return hexchat.EAT_ALL
-
-	nick = hexchat.get_info("nick")
-	chan = hexchat.get_info("channel")
-	userlist = hexchat.get_list("users")
+def getHost(uList, target):
+	for user in uList:
+		if user.nick.lower() == target.lower(): break
 	
-	try: line = random.choice(open(textfiledir + "\quotes.txt", "r").readlines())
-	except:
-		hexchat.emit_print("Notice", __module_name__ + " [S]", "Failed to grab a line from quotes.txt (Make sure it's in your config folder!), Using default reason if reason was not specified.")
-		line = "No reason specified."
-
-	for user in userlist:
-		if user.nick.lower() == word[1].lower(): break
-
-	host = user.host.partition('@')[2]
-
-	if user.nick.lower() == word[1].lower():	
-		hexchat.command("raw mode " + chan + " +b *!*@" + host)
-
-	else:
-		hexchat.emit_print("Notice", __module_name__ + " [S]", "User not found.")
-		return hexchat.EAT_ALL
-
-	try: reason = word_eol[2]
-	except: reason = line
-
-	reason = reason.replace('\n', '').replace('\r', '').replace('%k', word[1]).replace('%c', chan).replace('%u', nick)
-	hexchat.command("raw kick  " + chan + " " + word[1] + " " + ":" + reason + " [Banned]")
-
-	return hexchat.EAT_ALL
+	return user
 
 def kickquote(word, word_eol, userdata):
 	if len(word) <= 1:
 		hexchat.emit_print("Notice", __module_name__, "No arguments given.")
 		return hexchat.EAT_ALL
-	
+
 	nick = hexchat.get_info("nick")
 	chan = hexchat.get_info("channel")
 	userlist = hexchat.get_list("users")
+	user = getHost(userlist, word[1])
+	host = user.host.split('@')[1]
+	isBan = userdata
+	line = "No reason specified."
+
 	try: line = random.choice(open(textfiledir + "\quotes.txt", "r").readlines())
 	except:
-		hexchat.emit_print("Notice", __module_name__ + " [S]", "Failed to grab a line from quotes.txt (Make sure it's in your config folder!)-- Using default reason if reason was not specified.")
-		line = "No reason specified."
+		hexchat.emit_print("Notice", __module_name__ + " [S]", "Failed to grab a line from quotes.txt (Make sure it's in your config folder!), Using default reason if reason was not specified.")
+
+	if user.nick.lower() == word[1].lower() and isBan is not "":
+		hexchat.command("raw mode " + chan + " +b *!*@" + host)
 
 	try: reason = word_eol[2]
-	except IndexError: reason = line
-
-	for user in userlist:
-		if user.nick.lower() == word[1].lower(): break
+	except: reason = line
 
 	reason = reason.replace('\n', '').replace('\r', '').replace('%k', word[1]).replace('%c', chan).replace('%u', nick)
-	if user.nick.lower() == word[1].lower():
-		hexchat.command("raw kick  " + chan + " " + word[1] + " " + ":" + reason)
+	hexchat.command("raw kick  " + chan + " " + word[1] + " " + ":" + reason + "%s" % (isBan))
 
-	else: hexchat.emit_print("Notice", __module_name__ + " [S]", "User not found.")
 	return hexchat.EAT_ALL
 
-hexchat.hook_command("kick", kickquote, help="/kick Kicks a user.")
-hexchat.hook_command("kickban", kbquote, help="/kickban Kicks and bans a user.")
+hexchat.hook_command("kick", kickquote, userdata="", help="/kick Kicks a user.")
+hexchat.hook_command("kickban", kickquote, userdata=" [Banned]", help="/kickban Kicks and bans a user.")
 hexchat.emit_print("Notice", __module_name__ + " [S]", "%s by %s loaded. You are using version %s of the script." % (__module_name__, __author__, __module_version__))

e073918c3a599362c92e3ffdc700e4cab70fd21c
Erica Garcia <me@athenas.space>
2016-08-18 00:01:51 -0500

    command context now correlates to output context

--- whatpulse.py
+++ whatpulse.py
@@ -7,19 +7,20 @@ import requests, hexchat, threading
 
 __module_name__ = 'whatpulse spam'
 __module_description__ = 'spams whatpulse'
-__module_version__ = '0.0.1a'
+__module_version__ = '0.0.3a'
 __author__ = 'ApolloJustice'
 
 wpUsername = "ApolloJustice"
 
-def getData():
+def getData(chan):
 	wpData = requests.get("http://api.whatpulse.org/user.php?user={}&format=json".format(wpUsername)).json()
 	spamString = "WhatPulse Stats for \002\00302{}\017 | Keys: \002\00302{}\017 (Rank \002\00302{}\017) | Clicks: \002\00302{}\017 (Rank \002\00302{}\017) | DL: \002\00302{}\017 (Rank \002\00302{}\017) | UL: \002\00302{}\017 (Rank \002\00302{}\017) | Uptime: \002\00302{}\017 (Rank \002\00302{}\017) | Avg. Keys/s \002\00302{}\017 | Avg. Clicks/s \002\00302{}\017".format(wpData['AccountName'], wpData['Keys'], wpData['Ranks']['Keys'], wpData['Clicks'], wpData['Ranks']['Clicks'], wpData['Download'], wpData['Ranks']['Download'], wpData['Upload'], wpData['Ranks']['Upload'], wpData['UptimeShort'], wpData['Ranks']['Uptime'], wpData['AvKPS'], wpData['AvCPS'])
-	hexchat.command("say {}".format(spamString))
+	hexchat.command("msg {} {}".format(chan, spamString))
 
 def spamWP(word, word_eol, userdata):
-	dataThread = threading.Thread(target=getData)
+	chan = hexchat.get_info('channel')
+	dataThread = threading.Thread(target=getData, kwargs={"chan": chan})
 	dataThread.start()
-	return hexchat.EAT_ALL
+	return hexchat.EAT_ALL	
 
 hexchat.hook_command("whatpulse", spamWP)

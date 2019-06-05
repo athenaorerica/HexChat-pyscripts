e50021cba6382aa2b08166b3705a5206c5633a81
Erica Garcia <me@athenas.space>
2019-06-05 16:45:58 -0500

    Reformat and add todo to whatpulse.py

--- whatpulse.py
+++ whatpulse.py
@@ -19,17 +19,24 @@ __module_description__ = 'spams whatpulse'
 __module_version__ = '0.0.3a'
 __author__ = 'Erica Garcia [athenaorerica] <me@athenas.space>'
 
-wpUsername = "ApolloJustice"
+# TODO: make this more configurable, this is terrible.
+
+wpUsername = None
+
 
 def getData(chan):
-	wpData = requests.get("http://api.whatpulse.org/user.php?user={}&format=json".format(wpUsername)).json()
-	spamString = "WhatPulse Stats for \002\00302{}\017 | Keys: \002\00302{}\017 (Rank \002\00302{}\017) | Clicks: \002\00302{}\017 (Rank \002\00302{}\017) | DL: \002\00302{}\017 (Rank \002\00302{}\017) | UL: \002\00302{}\017 (Rank \002\00302{}\017) | Uptime: \002\00302{}\017 (Rank \002\00302{}\017) | Avg. Keys/s \002\00302{}\017 | Avg. Clicks/s \002\00302{}\017".format(wpData['AccountName'], wpData['Keys'], wpData['Ranks']['Keys'], wpData['Clicks'], wpData['Ranks']['Clicks'], wpData['Download'], wpData['Ranks']['Download'], wpData['Upload'], wpData['Ranks']['Upload'], wpData['UptimeShort'], wpData['Ranks']['Uptime'], wpData['AvKPS'], wpData['AvCPS'])
-	hexchat.command("msg {} {}".format(chan, spamString))
+    wpData = requests.get(
+        "http://api.whatpulse.org/user.php?user={}&format=json".format(wpUsername)).json()
+    spamString = "WhatPulse Stats for \002\00302{}\017 | Keys: \002\00302{}\017 (Rank \002\00302{}\017) | Clicks: \002\00302{}\017 (Rank \002\00302{}\017) | DL: \002\00302{}\017 (Rank \002\00302{}\017) | UL: \002\00302{}\017 (Rank \002\00302{}\017) | Uptime: \002\00302{}\017 (Rank \002\00302{}\017) | Avg. Keys/s \002\00302{}\017 | Avg. Clicks/s \002\00302{}\017".format(
+        wpData['AccountName'], wpData['Keys'], wpData['Ranks']['Keys'], wpData['Clicks'], wpData['Ranks']['Clicks'], wpData['Download'], wpData['Ranks']['Download'], wpData['Upload'], wpData['Ranks']['Upload'], wpData['UptimeShort'], wpData['Ranks']['Uptime'], wpData['AvKPS'], wpData['AvCPS'])
+    hexchat.command("msg {} {}".format(chan, spamString))
+
 
 def spamWP(word, word_eol, userdata):
-	chan = hexchat.get_info('channel')
-	dataThread = threading.Thread(target=getData, kwargs={"chan": chan})
-	dataThread.start()
-	return hexchat.EAT_ALL	
+    chan = hexchat.get_info('channel')
+    dataThread = threading.Thread(target=getData, kwargs={"chan": chan})
+    dataThread.start()
+    return hexchat.EAT_ALL
+
 
 hexchat.hook_command("whatpulse", spamWP)
